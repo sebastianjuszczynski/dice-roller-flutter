b6042f6ad6b4beb306ab6dd64306d729e4de522b
Sebastian Juszczyński <juszczynskiseb@agrolok.local>
2026-04-21 13:44:38 +0200

    Add full Flutter dice roller project

---
+++ lib/dice_roller.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var activeDiceImage = 'assets/images/dice-1.png';
+
+  void rollDice() {
+    setState(() {
+      activeDiceImage = 'assets/images/dice-3.png';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(activeDiceImage, width: 200, height: 200),
+        const SizedBox(height: 20),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 40,
+              vertical: 20,
+            ),
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text('Roll Dice'),
+        ),
+      ],
+    );
+  }
+}
